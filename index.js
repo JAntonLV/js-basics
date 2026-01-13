1976311cab52363610532019faeb9f2da1d0787d
JAntonLV <lopezv.joseantonio@gmail.com>
2026-01-13 03:21:35 -0600

    Add age validation logic

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("Mi primer commit");
+
+// Reto Día 1
+
+const nombre = "Antonio";
+const edad = 33;
+
+if (edad >= 18) {
+  console.log(`Hola ${nombre}, eres mayor de edad`);
+} else {
+  console.log(`Hola ${nombre}, eres menor de edad`);
+}
